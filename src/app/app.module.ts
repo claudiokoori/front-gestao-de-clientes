7cec44a3cc65890b2f96d57a053daa87e1bcd8ec
claudiokoori <claudiojrkoori@gmail.com>
2024-03-24 16:18:22 -0300

    Add Mascara de Formatacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Registro } from './registro/registro.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask  } from 'ngx-mask';
 
+
+
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,10 +23,14 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     NgbModule,
-    FormsModule
-    
+    FormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe
+      
   ],
-  providers: [],
+  providers: [
+    provideNgxMask()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
